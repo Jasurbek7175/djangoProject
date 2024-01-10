a0020d8f721f64d19333db57cc94c92421855302
user <jasurbekabdusalomov885@gmail.com>
2024-01-10 22:36:33 +0300

    models

--- restapp/models.py
+++ restapp/models.py
@@ -1,14 +1,13 @@
 # Create your models here.
 from django.db import models
 
-
 # Create your models here.
 class YoursDataModel(models.Model):
     name = models.CharField(max_length=255)
     age = models.IntegerField()
     city = models.CharField(max_length=255)
     contract_date = models.DateField()
-    created_at = models.DateField(auto_now_add=True)
+    created_at =models.DateField(auto_now_add=True)
 
 
 class FinanceModel(models.Model):
@@ -18,9 +17,3 @@ class FinanceModel(models.Model):
     contract_date = models.DateField()
     created_at = models.DateField(auto_now_add=True)
 
-
-class City(models.Model):
-    name = models.CharField(max_length=255)
-    area = models.IntegerField()
-    population = models.IntegerField()
-    created_at = models.DateField(auto_now_add=True)
